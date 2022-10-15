b9c4a6426b757c648794cd5ae7b7b5f2a61dfe13
Nishu Garg <nishu.garg@staqo.com>
2022-10-15 11:31:05 +0530

    pay now

--- src/pages/sprint_3/clinet-BookingPaymentpendingDetails.js
+++ src/pages/sprint_3/clinet-BookingPaymentpendingDetails.js
@@ -116,8 +116,8 @@ function ClientBookingPaymentpendingDetails () {
                             <div class="row mt-1">
                                 <div class="col-md-12">
                                     <div class="service-detailscont">
-                                        <h4>Service details</h4>
-                                        <p>You may have experience in fixing a lot of things at home but plumbing isn’t everyone’s cup of tea.
+                                        <h4>Invoice details</h4>
+                                        {/* <p>You may have experience in fixing a lot of things at home but plumbing isn’t everyone’s cup of tea.
                                             Plumbing issues such as clogged drains, leaky faucets, broken or damaged pipelines etc. can be a
                                             homeowner’s nightmare. Plumbing issues need to be addressed immediately in order to prevent them from
                                             exacerbating or causing further damages. Minor plumbing problems are quite common in every household.
@@ -125,8 +125,45 @@ function ClientBookingPaymentpendingDetails () {
                                             dripping tap, clogged shower head, cistern repair, toilet flush not working, clogged drain and
                                             sink or any other plumbing work. We have a huge network of plumbing contractors in
                                             <strong> Seattle, Washington</strong> to help you with all kinds of emergency plumbing services.</p>
-                                        <a href="#">read more <img src="images/arrow-deatil.svg" height="20px" alt="" /></a>
+                                        <a href="#">read more <img src="images/arrow-deatil.svg" height="20px" alt="" /></a> */}
                                     </div>
+                                    <div class="included-service p-3 mb-3 raiseinvoiceticketblue">
+                                        <div class="form-group mb-0">
+                                            <label for="#" class="profile-label mb-0">Reached Service Location <span class="text-muted jobtimig"><i class="far fa-calendar-alt"></i> 10/11/2022 | 15:00</span></label> 
+                                        </div>
+                                    </div>
+
+
+                                    <div className="included-service p-3 mb-3 raiseinvoiceticketgreen">
+
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <label for="#" class="profile-label">Job Started</label>
+                                            <span className="text-muted jobtimig"> <i class="far fa-calendar-alt"></i> 10/11/2022 | 15:00</span>
+                                            <div className="row">
+                                                <div className="col-md-8">
+                                                    <div class="Featured-img">
+                                                        <img src="images/furniture.jpg" alt="" class="furnitured-img" />
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div> 
+                                        <div className="col-md-6 mt-3 mt-sm-0">
+                                            <label for="#" class="profile-label">Completed</label>
+                                            <span className="text-muted jobtimig"> <i class="far fa-calendar-alt"></i> 10/11/2022 | 15:00</span>
+                                            <div className="row">
+                                                <div className="col-md-8">
+                                                    <div class="Featured-img">
+                                                        <img src="images/furniture.jpg" alt="" class="furnitured-img" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                   
 
                                     <div class="included-service mt-4">
                                         <div class="included-servicehead">
@@ -238,11 +275,8 @@ function ClientBookingPaymentpendingDetails () {
                                                 <div class="col-md-2 col-2 pl-4 pl-sm-5">
                                                     <h1>Sr. no.</h1>
                                                 </div>
-                                                <div class="col-md-5 col-5">
+                                                <div class="col-md-7 col-7">
                                                     <h1>Particulars</h1>
-                                                </div>
-                                                <div class="col-md-2 col-2">
-                                                    <h1>Qty</h1>
                                                 </div>
                                                 <div class="col-md-3 col-3 text-right pr-4 pr-sm-5">
                                                     <h1>Cost</h1>
@@ -253,11 +287,8 @@ function ClientBookingPaymentpendingDetails () {
                                             <div class="col-md-2 col-2 pl-4 pl-sm-5 ">
                                                 <p>01</p>
                                             </div>
-                                            <div class="col-md-5 col-5">
+                                            <div class="col-md-7 col-7">
                                                 <p>Drainage cleaning related activities</p>
-                                            </div>
-                                            <div class="col-md-2 col-2">
-                                                <p>x 1</p>
                                             </div>
                                             <div class="col-md-3 col-3 text-right pr-4 pr-sm-5">
                                                 <p>$109.00</p>
@@ -268,11 +299,8 @@ function ClientBookingPaymentpendingDetails () {
                                             <div class="col-md-2 pl-4 pl-sm-5 col-2">
                                                 <p>02</p>
                                             </div>
-                                            <div class="col-md-5 col-5">
+                                            <div class="col-md-7 col-7">
                                                 <p>Grouting repair work</p>
-                                            </div>
-                                            <div class="col-md-2 col-2">
-                                                <p>x 2</p>
                                             </div>
                                             <div class="col-md-3 text-right pr-4 pr-sm-5 col-3">
                                                 <p>$109.00</p>
@@ -283,11 +311,8 @@ function ClientBookingPaymentpendingDetails () {
                                             <div class="col-md-2 pl-4 pl-sm-5 col-2">
                                                 <p>03</p>
                                             </div>
-                                            <div class="col-md-5 col-5">
+                                            <div class="col-md-7 col-7">
                                                 <p>Light cement work</p>
-                                            </div>
-                                            <div class="col-md-2 col-2">
-                                                <p>x 1</p>
                                             </div>
                                             <div class="col-md-3 col-3 text-right pr-4 pr-sm-5">
                                                 <p>$109.00</p>
@@ -297,11 +322,8 @@ function ClientBookingPaymentpendingDetails () {
                                             <div class="col-md-2 pl-4 pl-sm-5 col-2">
                                                 <p>04</p>
                                             </div>
-                                            <div class="col-md-5 col-5">
+                                            <div class="col-md-7 col-7">
                                                 <p>Plumbing related consultation</p>
-                                            </div>
-                                            <div class="col-md-2 col-2">
-                                                <p>x 4</p>
                                             </div>
                                             <div class="col-md-3 text-right pr-4 pr-sm-5 col-3">
                                                 <p>$109.00</p>
@@ -314,27 +336,24 @@ function ClientBookingPaymentpendingDetails () {
                                     <div class="border-bottom mt-4"></div>
 
                                     <div class="service-detailscont mt-3 mt-sm-4">
-                                        <h4>About the company</h4>
+                                        <h4>Remarks</h4>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
                                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                           aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                                           dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                                           officia deserunt mollit anim id est laborum.</p>
-                                        <a href="#">read more <img src="images/arrow-deatil.svg" height="20px" alt="" /></a>
+                                           aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </p> 
                                     </div>
 
-                                    <div class="border-bottom mt-4"></div>
+                                    {/* <div class="border-bottom mt-4"></div> */}
 
-                                    <div class="row mt-3 mt-sm-4">
+                                    {/* <div class="row mt-3 mt-sm-4">
                                         <div class="col-md-12">
                                             <div class="service-detailscont">
                                                 <h4>Reviews</h4>
                                                 <h5><img src="images/star-r.svg" class="mr-1" alt="" height="15px" />4.7 <span>(147 reviews)</span></h5>
                                             </div>
                                         </div>
-                                    </div>
+                                    </div> */}
 
-                                    <div class="reviews-rating mt-2 ">
+                                    {/* <div class="reviews-rating mt-2 ">
                                         <div class="row">
                                             <div class="col-md-2 col-6">
                                                 <p>Cooperation</p>
@@ -376,9 +395,9 @@ function ClientBookingPaymentpendingDetails () {
                                                 <h4>1 stars</h4>
                                             </div>
                                         </div>
-                                    </div>
+                                    </div> */}
 
-                                    <div class="review-providerSec mt-3 mt-sm-4">
+                                    {/* <div class="review-providerSec mt-3 mt-sm-4">
                                         <h1>Reviews by previous service providers</h1>
                                         <div class="border-bottom mt-2 mt-sm-3"></div>
                                         <div class="row mt-4">
@@ -498,7 +517,7 @@ function ClientBookingPaymentpendingDetails () {
                                             </div>
                                         </div>
 
-                                    </div>
+                                    </div> */}
 
                                 </div>
                             </div>
@@ -515,8 +534,8 @@ function ClientBookingPaymentpendingDetails () {
                                     </div>
 
                                     <div class="row align-items-end">
-                                        <div class="col-md-12 mb-3">
-                                            <h2>INVOICE</h2>
+                                        <div class="col-md-12">
+                                            <h2>Remaining Payable</h2>
                                         </div>
                                         <div class="col-md-8 col-8">
                                             <h1 >$730.00</h1>
@@ -526,38 +545,60 @@ function ClientBookingPaymentpendingDetails () {
 
                                     <div class="dashed-borderfixedbox mt-4"></div>
 
-                                    <div class="row  pt-3">
-                                        <div class="col-md-4 col-3">
+                                    <div class="row pt-3">
+                                        <div class="col-8">
                                             <h4>Duration</h4>
                                         </div>
-                                        <div class="col-md-8 text-right col-9">
-                                            <h4>3 hrs 15 min</h4>
+                                        <div class="text-right col-4">
+                                            <h4>1.25 HR</h4>
                                         </div>
                                     </div>
                                     <div class="row mt-3">
-                                        <div class="col-md-5 col-5">
-                                            <h4>Visiting charges</h4>
+                                    <div class="col-8">
+                                            <h4>Service Cost (Fixed)</h4>
                                         </div>
-                                        <div class="col-md-7 text-right col-7">
-                                            <h4>- $20.00</h4>
-                                        </div>
-                                    </div>
-                                    <div class="row mt-3">
-                                        <div class="col-md-8 col-8">
-                                            <h4>Extra (by service provider)</h4>
-
-                                        </div>
-                                        <div class="col-md-4 text-right col-4">
-                                            <h4>$288.00</h4>
+                                        <div class="text-right col-4">
+                                            <h4>$100</h4>
                                         </div>
                                     </div>
                                     <div class="row mt-3">
-                                        <div class="col-md-8 col-8">
-                                            <h4>Applicable taxes (@10%)</h4>
-                                            <h5>GST, VAT all incl.</h5>
+                                        <div class="col-8">
+                                            <h4>Extra Charges</h4> 
                                         </div>
-                                        <div class="col-md-4 text-right col-4">
-                                            <h4>$75.00</h4>
+                                        <div class="text-right col-4">
+                                            <h4>$20</h4>
+                                        </div>
+                                    </div>
+                                    <div class="row mt-3">
+                                        <div class="col-8">
+                                            <h4><strong>Total Service Cost</strong></h4>
+                                        </div>
+                                        <div class="text-right col-4">
+                                            <h4>$120</h4>
+                                        </div>
+                                    </div>
+                                    <div class="row mt-3">
+                                        <div class="col-8">
+                                            <h4>Taxes Payable @ 25%</h4>
+                                        </div>
+                                        <div class="text-right col-4">
+                                            <h4>$30</h4>
+                                        </div>
+                                    </div>
+                                    <div class="row mt-3">
+                                        <div class="col-8">
+                                            <h4><strong>Grand Total</strong></h4>
+                                        </div>
+                                        <div class="text-right col-4">
+                                            <h4>$150</h4>
+                                        </div>
+                                    </div>
+                                    <div class="row mt-3">
+                                        <div class="col-8">
+                                            <h4>Visiting Charges Paid</h4>
+                                        </div>
+                                        <div class="text-right col-4">
+                                            <h4>-$20</h4>
                                         </div>
                                     </div>
                                 </div>
